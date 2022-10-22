7db33cb19a7fcff04b952aa9f3140dfa0b153de4
Martin Chien <martinchien92@gmail.com>
2022-10-22 22:16:36 +0200

    completed lab

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
           <div className="container">
               <Routes>
                   <Route index element={<Labs/>}/>
+                  <Route path="/*" element={<Labs/>}/>
                   <Route path="/hello" element={<HelloWorld/>}/>
                   <Route path="/tuiter/*" element={<Tuiter/>}/>
               </Routes>
